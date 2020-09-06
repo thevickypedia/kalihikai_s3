c1357fe672986e194c2a5359677e35b2b6b1e90c
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-05 19:10:02 -0500

    pop up page before the actual page

---
+++ initial.js
@@ -0,0 +1,9 @@
+function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
+    else document.getElementById('ac-wrapper').removeAttribute('style');
+}
+window.onload = function () {
+    setTimeout(function () {
+        PopUp('show');
+    }, 5);
+}
